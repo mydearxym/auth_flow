2ec3562080eea84065783f70bc73a7af66c67886
mydearxym <mydearxym@qq.com>
2019-04-09 10:49:54 +0800

    chore(page): cp sign_in to index

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,23 @@
 import React from 'react'
-// import { Provider } from 'mobx-react'
+import { Provider } from 'mobx-react'
 
-// import ThemeWrapper from 'containers/ThemeWrapper'
-// import MultiLanguage from 'containers/MultiLanguage'
+import ThemeWrapper from 'containers/ThemeWrapper'
 // import Preview from 'containers/Preview'
-// import Route from 'containers/Route'
-/*
-   import AuthLayout from 'components/AuthLayout'
-   import AuthBanner from 'components/AuthBanner'
+import AuthLayout from 'components/AuthLayout'
+import AuthBanner from 'components/AuthBanner'
 
-   import SigninForm from 'containers/SigninForm'
-   import Footer from 'containers/Footer'
- */
+import SigninForm from 'containers/SigninForm'
+import Footer from 'containers/Footer'
 
-// import GAWraper from 'components/GAWraper'
+import GAWraper from 'components/GAWraper'
 // import ErrorPage from 'components/ErrorPage'
-// import initRootStore from 'stores/init'
-// import { GAWraper, ErrorPage } from 'components'
+import initRootStore from 'stores/init'
 
 import { makeDebugger } from 'utils'
 
 /* eslint-disable-next-line */
 const debug = makeDebugger('page:community')
 
-// try to fix safari bug
-// see https://github.com/yahoo/react-intl/issues/422
-// global.Intl = require('intl')
-/*
-   NOTE: in dev mode, this index page is always be required, even the server
-   is not routing to this page, it's very confused, help needed
-
-   currently it's just the community page with no data fetch, works fine though
- */
 export default class SignIn extends React.Component {
   static async getInitialProps() {
     // const mainPath = getMainPath(props)
@@ -40,7 +26,6 @@ export default class SignIn extends React.Component {
     return {}
   }
 
-  /*
   constructor(props) {
     super(props)
     const store = props.statusCode
@@ -48,12 +33,23 @@ export default class SignIn extends React.Component {
       : initRootStore({ ...props })
 
     this.store = store
+    // this.store = initRootStore({ ...props })
   }
-  */
 
   render() {
     // const { statusCode, target } = this.props
-
-    return <h3>Hello world 34</h3>
+    return (
+      <Provider store={this.store}>
+        <GAWraper>
+          <ThemeWrapper>
+            <AuthLayout>
+              <AuthBanner />
+              <SigninForm />
+              <Footer />
+            </AuthLayout>
+          </ThemeWrapper>
+        </GAWraper>
+      </Provider>
+    )
   }
 }
